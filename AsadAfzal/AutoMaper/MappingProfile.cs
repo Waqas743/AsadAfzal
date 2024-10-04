670a17e0d8b62bccdcc77f1fd494a189d8a5501b
Waqas Abbas <waqas.abbas@sdsoltechnologies.com>
2024-10-04 14:23:07 +0500

    Configure Automapper

---
+++ AsadAfzal/AutoMaper/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AsadAfzal.Models;
+using AutoMapper;
+
+namespace AsadAfzal.AutoMaper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+        }
+    }
+}
--- AsadAfzal/Program.cs
+++ AsadAfzal/Program.cs
@@ -1,8 +1,10 @@
+using AsadAfzal.AutoMaper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +25,4 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
